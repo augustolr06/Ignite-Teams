869daa7468e3e9ef70abb1c51b9248168851cb58
Augusto Lázaro <augusto.lr06@gmail.com>
2023-04-03 11:56:06 -0300

    feat: add font loader and Loading to waiting

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { View, StatusBar } from 'react-native'
-import { Groups } from './src/screens/Groups'
-import { ThemeProvider } from 'styled-components'
+import { StatusBar } from 'react-native'
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
 
+import { Groups } from './src/screens/Groups'
+import { Loading } from '@components/Loading'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
       <ThemeProvider theme={theme}>
         <StatusBar
@@ -13,7 +22,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        <Groups />
+        {fontsLoaded ? <Groups /> : <Loading />}
       </ThemeProvider>
   );
 }
