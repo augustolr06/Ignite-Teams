93c67b9d9b712da2de33958f92ba619375db763d
Augusto Lázaro <augusto.lr06@gmail.com>
2023-04-03 11:53:05 -0300

    feat: add Header, Highlight and groups in Flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,35 @@
-import { Container, Title } from './styles'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
+import { ListEmpty } from '@components/ListEmpty'
+import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocketseat', 'Galera do Ignite', 'Galera do Next Level Week'])
+
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
+      <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhuma turma encontrada. Que tal cadastrar a primeira turma?" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={groups.length === 0 && 
+          { flex: 1, paddingBottom: 24 }}
+      />
+
     </Container>
   )
 }
