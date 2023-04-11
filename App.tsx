d2324ecfacba191b3d98a76cdbdac443cb9011b4
Augusto Lazaro <augusto.lr06@gmail.com>
2023-04-11 11:22:34 -0300

    Refactor: change from just one screen to routes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'react-native'
 
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './src/theme'
 
-import { Groups } from '@screens/Groups'
-import { NewGroup } from '@screens/NewGroup'
 import { Loading } from '@components/Loading'
-import { Players } from '@screens/Players'
+import { Routes } from '@routes/index'
 
 export default function App() {
 
@@ -24,7 +22,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Players /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </ThemeProvider>
   );
 }
