5cd96c2f4ef2dc395e7852ee0183a257c8701acd
Augusto Lazaro <augusto.lr06@gmail.com>
2023-04-12 10:49:31 -0300

    feat: Implement back button behavior

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { TouchableOpacity } from 'react-native'
 import logoImg from '@assets/logo.png'
 import { BackIcon, Container, Logo } from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 interface HeaderProps {
   hasBackButton?: boolean
@@ -8,10 +9,15 @@ interface HeaderProps {
 
 export function Header(props: HeaderProps) {
   const { hasBackButton = false } = props
+  const navigation = useNavigation()
+
+  function handleGoHome() {
+    navigation.navigate('groups')
+  }
   return (
     <Container hasBackButton={hasBackButton}>
       {hasBackButton && (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleGoHome} >
           <BackIcon />
         </TouchableOpacity>
       )}
